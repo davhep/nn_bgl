c415ddd49b108fa199471d5d8bf0b66f86179566
Alexandr Dmitriev <alexandrvdmitriev@gmail.com>
2020-02-04 17:07:10 +0300

    More logilcal iteration procedure in analyzing part

--- src/analyze_model.cpp
+++ src/analyze_model.cpp
@@ -54,46 +54,50 @@ int main()
 	
 	ofstream data_dump("model_vs_practice.txt");
 	boost::graph_traits<Graph>::edge_iterator ei, ei_end;
-	for (boost::tie(ei, ei_end) = boost::edges(myNet.m_net_graph); ei != ei_end; ++ei){
-			auto source = boost::source ( *ei, myNet.m_net_graph);
-			auto target = boost::target ( *ei, myNet.m_net_graph);
-			vector<double> m_deltas, weights;
-			while(!trainData.isEof()){
-				// Get new input data and feed it forward:
-				assert(trainData.getNextInputs(inputVals) == topology[0]);
-				myNet.feedForward(inputVals);
-				dumpVectorVals("inputVals", data_dump, inputVals);
-				// Collect the net's actual results:
-				myNet.getResults(resultVals);
-				// Train the net what the outputs should have been:
-				trainData.getTargetOutputs(targetVals);
-				if(debug_high)
-				{
-					cout << "Pass" << trainingPass << endl;
-					showVectorVals("Inputs :", inputVals);
-					showVectorVals("Outputs:", resultVals);
-					showVectorVals("Targets:", targetVals);
-				}	
-				assert(targetVals.size() == topology.back());	
-				myNet.backProp(targetVals, false); // don not upgrade weigths to avoid model change while analyze
+	std::map<std::pair<int,int>,vector<double>>	m_deltas, weights;		
+	while(!trainData.isEof()){
+		// Get new input data and feed it forward:
+		assert(trainData.getNextInputs(inputVals) == topology[0]);
+		myNet.feedForward(inputVals);
+		dumpVectorVals("inputVals", data_dump, inputVals);
+		// Collect the net's actual results:
+		myNet.getResults(resultVals);
+		// Train the net what the outputs should have been:
+		trainData.getTargetOutputs(targetVals);
+		if(debug_high)
+		{
+			cout << "Pass" << trainingPass << endl;
+			showVectorVals("Inputs :", inputVals);
+			showVectorVals("Outputs:", resultVals);
+			showVectorVals("Targets:", targetVals);
+		}	
+		assert(targetVals.size() == topology.back());	
+		myNet.backProp(targetVals, false); // don not upgrade weigths to avoid model change while analyze
+		
+		for (boost::tie(ei, ei_end) = boost::edges(myNet.m_net_graph); ei != ei_end; ++ei){
+				auto source = boost::source ( *ei, myNet.m_net_graph);
+				auto target = boost::target ( *ei, myNet.m_net_graph);
 				std::pair<edge_descriptor,bool> edge_saved = boost::edge(source, target, myNet.m_net_graph);
 				assert(edge_saved.second == true); //because we _must_ have edge in saved graph, if one exists in current graph
 				//cout << dumped_net.m_net_graph[edge_saved.first].m_delta_weight << endl;
-				weights.push_back(myNet.m_net_graph[edge_saved.first].m_weight);
-				m_deltas.push_back(myNet.m_net_graph[edge_saved.first].m_delta_weight);
-			}
-			trainData.reset();
-			//showVectorVals( "Weights", weights);
-			//showVectorVals("m_deltas", m_deltas);
-							
-			double sum_weights = std::accumulate(weights.begin(), weights.end(), 0.0);
-			double mean_weights = sum_weights / weights.size();
-			
-			double sum_deltaw = std::accumulate(m_deltas.begin(), m_deltas.end(), 0.0);
-			double mean_deltaw = sum_deltaw / m_deltas.size();
-			
-			double sq_sum = std::inner_product(m_deltas.begin(), m_deltas.end(), m_deltas.begin(), 0.0);
-			double stdev = std::sqrt(sq_sum / m_deltas.size() - mean_deltaw * mean_deltaw);
-			cout << source << " to " << target << " W= " << mean_weights << " d= " << mean_deltaw << "	d_var=	" << stdev << " d_var/W= " << stdev/mean_weights << endl;
+				weights[std::make_pair(source,target)].push_back(myNet.m_net_graph[edge_saved.first].m_weight);
+				m_deltas[std::make_pair(source,target)].push_back(myNet.m_net_graph[edge_saved.first].m_delta_weight);
+		}
+	}
+
+	for (boost::tie(ei, ei_end) = boost::edges(myNet.m_net_graph); ei != ei_end; ++ei){
+		auto source = boost::source ( *ei, myNet.m_net_graph);
+		auto target = boost::target ( *ei, myNet.m_net_graph);
+		auto weights_vec = weights[std::make_pair(source,target)];
+	    auto m_deltas_vec = m_deltas[std::make_pair(source,target)];	
+		double sum_weights = std::accumulate(weights_vec.begin(), weights_vec.end(), 0.0);
+		double mean_weights = sum_weights / weights.size();
+		
+		double sum_deltaw = std::accumulate(m_deltas_vec.begin(), m_deltas_vec.end(), 0.0);
+		double mean_deltaw = sum_deltaw / m_deltas_vec.size();
+		
+		double sq_sum = std::inner_product(m_deltas_vec.begin(), m_deltas_vec.end(), m_deltas_vec.begin(), 0.0);
+		double stdev = std::sqrt(sq_sum / m_deltas_vec.size() - mean_deltaw * mean_deltaw);
+		cout << source << " to " << target << " W= " << mean_weights << " d= " << mean_deltaw << "	d_var=	" << stdev << " d_var/W= " << stdev/mean_weights << endl;
 	}
 }
